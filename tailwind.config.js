983c96ccaf21bf163a2390296829532631b3428d
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-01-21 20:15:12 +0600

    Feature Product Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1140px'
-    },
+    // screens: {
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1140px'
+    // },
     container:{
       center: 'true',
       padding:{
