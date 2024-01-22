05cbbf9bfcf3b9a342f52e47d9bd8f3f61f5666e
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-01-22 10:30:55 +0600

    Great Tea Items Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,10 +26,20 @@ module.exports = {
       fontFamily:{
         primary: 'Manrope',
         secondary: 'Poppins',
-      },
+      }, 
       boxShadow:{
         ratings: '0px 7px 50px 0px rgba(0, 0, 0, 0.06)'
-      }
+      },
+      backgroundColor:{
+        'great-tea-2': 'rgba(230, 166, 35, 0.10)',
+        'great-tea-3': 'rgba(87, 118, 57, 0.10)',
+      },
+      backgroundImage:{
+        'btn-bg': 'linear-gradient(270deg, #F00 0%, #FF8938 100%)',
+        'feature-product': 'linear-gradient(180deg, #F4F4F4 0%, rgba(244, 244, 244, 0.00) 100%)',
+        'great-tea-1': 'linear-gradient(90deg, #e1b96f94 50%, rgba(255, 255, 255, 0.00) 100%)',
+        'great-tea-4': 'linear-gradient(90deg, #D9D9D9 0%, rgba(255, 255, 255, 0.00) 100%)',
+      },
     },
   },
   plugins: [],
